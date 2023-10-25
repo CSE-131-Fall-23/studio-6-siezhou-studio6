7d3954eb9f07d974edf9da895a647dab7ac90d32
Walpang <58585553+Walpang@users.noreply.github.com>
2023-10-25 16:49:52 -0500

    circles upon circles and geometric sum (to reversed wip)

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,9 +12,16 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
+		//sum = 1/2^n + 1/2^n-1...1/2
+		//base case n=0
+		double sum = Math.pow(0.5, n);
+		if (n==0) {
 			return 0;
+		}
+		else {
+			return sum + geometricSum(n-1);
+		}
+		
 		
 	}
 
@@ -42,9 +49,21 @@ public class RecursiveMethods {
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
-		
+		int i = 0;
+		int[] reverse = new int[array.length];
+		toReversedHelper(array, reverse, i);
 			// FIXME create a helper method that can recursively reverse the given array
 			return new int[0];
+	}	
+	public static int[] toReversedHelper(int[] array, int[] reverse, int i) {
+		if (i == array.length) {
+			return reverse;
+		} else {
+			reverse[reverse.length-1 - i] = array[i];
+			return toReversedHelper(array, reverse, i+1);
+		}
+		
+		
 		
 	}
 
@@ -59,8 +78,14 @@ public class RecursiveMethods {
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
 			double radiusMinimumDrawingThreshold) {
+		if (radius > radiusMinimumDrawingThreshold){
+		StdDraw.circle(xCenter, yCenter, radius);
 		
-		// FIXME
+		circlesUponCircles(xCenter-radius, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+		circlesUponCircles(xCenter+radius, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+		circlesUponCircles(xCenter, yCenter-radius, radius/3.0, radiusMinimumDrawingThreshold);
+		circlesUponCircles(xCenter, yCenter+radius, radius/3.0, radiusMinimumDrawingThreshold);
+		}
 	}
 
 }
